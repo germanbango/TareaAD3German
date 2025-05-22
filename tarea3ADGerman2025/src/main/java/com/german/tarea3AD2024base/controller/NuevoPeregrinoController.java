fa67f50eb3424d1882931881f59d48823b77af95
Germán <Germn@DESKTOP-OOC1KPQ.station>
2025-05-22 14:17:18 +0200

    commit 2

--- tarea3ADGerman2025/src/main/java/com/german/tarea3AD2024base/controller/NuevoPeregrinoController.java
+++ tarea3ADGerman2025/src/main/java/com/german/tarea3AD2024base/controller/NuevoPeregrinoController.java
@@ -1,12 +1,27 @@
 package com.german.tarea3AD2024base.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Controller;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import com.german.tarea3AD2024base.config.StageManager;
+import com.german.tarea3AD2024base.services.ParadaServicio;
 import com.german.tarea3AD2024base.view.FxmlView;
 
 import javafx.fxml.FXML;
@@ -14,8 +29,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-public class NuevoPeregrinoController{
-	
+@Controller
+public class NuevoPeregrinoController implements Initializable {
+
 	@FXML
 	private ComboBox<String> cmbParadas;
 	@FXML
@@ -31,8 +47,52 @@ public class NuevoPeregrinoController{
 	@Autowired
 	private StageManager stageManager;
 
+	@Autowired
+	private ParadaServicio paradaServicio;
+
+	private List<String> paradas;
+	private List<String> paises;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		paradas = paradaServicio.nombreyRegiones();
+		cmbParadas.getItems().addAll(paradas);
+		paises = cargarPaises();
+		cmbNacionalidad.getItems().addAll(paises);
+
+	}
+
 	@FXML
-	private void registrarPeregrino(){
+	private void salir() {
 		stageManager.switchScene(FxmlView.LOGIN);
 	}
+
+	private List<String> cargarPaises() {
+		List<String> ret = new ArrayList<>();
+		File paises = new File("src/main/resources/paises.xml");
+
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse(paises);
+			doc.getDocumentElement().normalize();
+			NodeList listapaises = doc.getElementsByTagName("pais");
+
+			for (int i = 0; i < listapaises.getLength(); i++) {
+				Node nodo = listapaises.item(i);
+
+				if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+					Element pais = (Element) nodo;
+
+					String nombrep = pais.getElementsByTagName("nombre").item(0).getTextContent();
+					ret.add(nombrep);
+				}
+			}
+
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+		return ret;
+	}
+
 }
--- tarea3ADGerman2025/src/main/java/com/german/tarea3AD2024base/services/ParadaServicio.java
+++ tarea3ADGerman2025/src/main/java/com/german/tarea3AD2024base/services/ParadaServicio.java
@@ -1,5 +1,6 @@
 package com.german.tarea3AD2024base.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,42 +9,50 @@ import org.springframework.stereotype.Service;
 import com.german.tarea3AD2024base.modelo.Parada;
 import com.german.tarea3AD2024base.repositorios.ParadaRepositorio;
 
-
 @Service
 public class ParadaServicio {
-	
+
 	@Autowired
 	private ParadaRepositorio paradaRepositorio;
-	
+
 	public Parada guardar(Parada entity) {
 		return paradaRepositorio.save(entity);
 	}
-	
+
 	public Parada actualizar(Parada entity) {
 		return paradaRepositorio.save(entity);
 	}
-	
-	public void borrar(Parada entity ) {
+
+	public void borrar(Parada entity) {
 		paradaRepositorio.delete(entity);
 	}
-	
+
 	public void borrarPorId(Long id) {
 		paradaRepositorio.deleteById(id);
 	}
-	
+
 	public void borrarPorLote(List<Parada> paradas) {
 		paradaRepositorio.deleteAll(paradas);
 	}
-	
+
 	public Parada encontrarPorId(Long id) {
 		return paradaRepositorio.findById(id).get();
 	}
-	
+
 	public List<Parada> encontrarTodos() {
 		return paradaRepositorio.findAll();
 	}
-	 public boolean existeParadaConNombreYRegion(String nombre, char region) {
-	        return paradaRepositorio.existsByNombreAndRegion(nombre, region);
-	    }
+
+	public boolean existeParadaConNombreYRegion(String nombre, char region) {
+		return paradaRepositorio.existsByNombreAndRegion(nombre, region);
+	}
+	
+	public List<String> nombreyRegiones() {
+		List<String> ret = new ArrayList<>();
+		for (Parada parada : encontrarTodos()) {
+			ret.add(parada.getNombre() + " (" + parada.getRegion() + ")");
+		}
+		return ret;
+	}
 
 }
