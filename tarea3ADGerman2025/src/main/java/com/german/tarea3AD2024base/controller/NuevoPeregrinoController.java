6e89c5dd1e181965b66e137df99b953daae68c56
german bango <92051496+germanbango@users.noreply.github.com>
2025-05-22 20:00:13 +0200

    Update NuevoPeregrinoController.java

--- tarea3ADGerman2025/src/main/java/com/german/tarea3AD2024base/controller/NuevoPeregrinoController.java
+++ tarea3ADGerman2025/src/main/java/com/german/tarea3AD2024base/controller/NuevoPeregrinoController.java
@@ -3,6 +3,7 @@ package com.german.tarea3AD2024base.controller;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -21,7 +22,16 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import com.german.tarea3AD2024base.config.StageManager;
+import com.german.tarea3AD2024base.modelo.Carnet;
+import com.german.tarea3AD2024base.modelo.Parada;
+import com.german.tarea3AD2024base.modelo.Peregrino;
+import com.german.tarea3AD2024base.modelo.PeregrinoParada;
+import com.german.tarea3AD2024base.modelo.Rol;
+import com.german.tarea3AD2024base.modelo.User;
+import com.german.tarea3AD2024base.services.CarnetServicio;
 import com.german.tarea3AD2024base.services.ParadaServicio;
+import com.german.tarea3AD2024base.services.PeregrinoParadaServicio;
+import com.german.tarea3AD2024base.services.PeregrinoServicio;
 import com.german.tarea3AD2024base.services.UserService;
 import com.german.tarea3AD2024base.view.FxmlView;
 
@@ -30,6 +40,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 @Controller
@@ -44,7 +55,7 @@ public class NuevoPeregrinoController implements Initializable {
 	@FXML
 	private TextField txtUsuario;
 	@FXML
-	private TextField txtContrasena;
+	private PasswordField txtContrasena;
 
 	@Lazy
 	@Autowired
@@ -52,9 +63,18 @@ public class NuevoPeregrinoController implements Initializable {
 
 	@Autowired
 	private ParadaServicio paradaServicio;
-	
+
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private PeregrinoServicio peregrinoServicio;
+	
+	@Autowired
+	private CarnetServicio carnetServicio;
+	
+	@Autowired
+	private PeregrinoParadaServicio peregrinoParadaServicio;
 
 	private List<String> paradas;
 	private List<String> paises;
@@ -67,34 +87,80 @@ public class NuevoPeregrinoController implements Initializable {
 		cmbNacionalidad.getItems().addAll(paises);
 
 	}
+	
+	@FXML
+	public void GuardarPeregrino() {
+		if(validar()) {
+			Peregrino peregrino = new Peregrino();
+			Parada paradaInicial = new Parada();
+			Carnet carnet = new Carnet();
+			PeregrinoParada peregrinoParada = new PeregrinoParada();
+			User usuario = new User();
+			crearPeregrino(usuario,peregrino, paradaInicial, carnet, peregrinoParada);
+			mostrarAlerta(AlertType.CONFIRMATION, "Peregrino creado correctamente", "Se ha podido crear el peregrino sin probelmas");
+		}
+	}
+
+	private void crearPeregrino(User usuario,Peregrino peregrino, Parada paradaInicial, Carnet carnet, PeregrinoParada peregrinoParada) {
+		String usuarionuevo = txtUsuario.getText();
+		String contrasena = txtContrasena.getText();
+		String nombre = txtNombrePeregrino.getText();
+		String nacionalidad = cmbNacionalidad.getValue();
+		String paradaSeleccionada = cmbParadas.getValue();
+		String[] partes = paradaSeleccionada.split("  ");
+		String parada = partes[0];
+		char region = partes[1].charAt(0);
+		usuario.setEmail(usuarionuevo);
+		usuario.setPassword(contrasena);
+		usuario.setRol(Rol.PEREGRINO);
+		userService.save(usuario);
+		paradaInicial = paradaServicio.paradaPorNombreyRegion(parada, region);
+		peregrino.setNombre(nombre);
+		peregrino.setNacionalidad(nacionalidad);
+		peregrino.setUsuario(usuario);
+		peregrinoServicio.guardar(peregrino);
+		carnet.setDistancia(0.0);
+		carnet.setFechaexp(LocalDate.now());
+		carnet.setNvips(0);
+		carnet.setParadaInicial(paradaInicial);
+		carnet.setPeregrino(peregrino);
+		carnetServicio.guardar(carnet);
+		peregrinoParada.setParada(paradaInicial);
+		peregrinoParada.setPeregrino(peregrino);
+		peregrinoParadaServicio.guardar(peregrinoParada);
+
+	}
 
 	@FXML
 	private void salir() {
 		stageManager.switchScene(FxmlView.LOGIN);
 	}
-	
+
 	private boolean validar() {
-		
+
 		String nombre = txtNombrePeregrino.getText();
 		String parada = cmbParadas.getValue();
 		String nacionalidad = cmbNacionalidad.getValue();
 		String usuario = txtUsuario.getText();
 		String contrasena = txtContrasena.getText();
-		
-		if(nombre.isEmpty()) {
+
+		if (nombre.isEmpty()) {
 			mostrarAlerta(AlertType.ERROR, "ERROR:", "debe introducir su nombre");
 			return false;
 		}
-		if(parada.isEmpty()||nacionalidad.equals(null)){
+		if (parada.isEmpty() || nacionalidad.equals(null)) {
 			mostrarAlerta(AlertType.ERROR, "ERROR:", "debe seleccionar una parada");
 			return false;
-		}if(usuario.isEmpty()||usuario.contains(" ")||userService.existeUsuarioConEmail(usuario)){
+		}
+		if (usuario.isEmpty() || usuario.contains(" ") || userService.existeUsuarioConEmail(usuario)) {
 			mostrarAlerta(AlertType.ERROR, "ERROR:", "formato de usuario incorrecto o ya existe");
 			return false;
-		}if(contrasena.isEmpty()||contrasena.contains(" ")) {
+		}
+		if (contrasena.isEmpty() || contrasena.contains(" ")) {
 			mostrarAlerta(AlertType.ERROR, "ERROR:", "formato de contrasena incorrecto");
 			return false;
-		}if(nacionalidad.isEmpty()|| nacionalidad.equals(null)){
+		}
+		if (nacionalidad.isEmpty() || nacionalidad.equals(null)) {
 			mostrarAlerta(AlertType.ERROR, "ERROR:", "debe seleccionar una nacionalidad");
 			return false;
 		}
@@ -128,13 +194,13 @@ public class NuevoPeregrinoController implements Initializable {
 		}
 		return ret;
 	}
-	
+
 	private void mostrarAlerta(AlertType tipo, String titulo, String mensaje) {
-        Alert alert = new Alert(tipo);
-        alert.setTitle(titulo);
-        alert.setHeaderText(null);
-        alert.setContentText(mensaje);
-        alert.showAndWait();
-    }
+		Alert alert = new Alert(tipo);
+		alert.setTitle(titulo);
+		alert.setHeaderText(null);
+		alert.setContentText(mensaje);
+		alert.showAndWait();
+	}
 
 }
